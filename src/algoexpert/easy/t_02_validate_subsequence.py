af7666ebb3715bf8dffbdbf007c55e7fdbfdc37f
Piotr Kosmowski <kospiotr@gmail.com>
2025-07-26 21:34:09 +0200

    Update

---
+++ src/algoexpert/easy/t_02_validate_subsequence.py
@@ -0,0 +1,77 @@
+"""
+Given two non-empty arrays of integers, write a function that determines
+whether the second array is a subsequence of the first one.
+
+
+A subsequence of an array is a set of numbers that aren't necessarily adjacent
+in the array but that are in the same order as they appear in the array. For
+instance, the numbers [1, 3, 4] form a subsequence of the array
+[1, 2, 3, 4], and so do the numbers [2, 4]. Note
+that a single number in an array and the array itself are both valid
+subsequences of the array.
+
+Sample Input
+array = [5, 1, 22, 25, 6, -1, 8, 10]
+sequence = [1, 6, -1, 10]
+
+Sample Output
+true
+"""
+
+
+# noinspection PyPep8Naming
+def isValidSubsequence_sol_1(array, sequence):
+    """
+    Brute force solution
+    """
+
+    if len(array) < len(sequence):
+        return False
+
+    l_pointer = 0
+    r_pointer = 0
+    s_pointer = 0
+
+    print(array, sequence)
+
+    def arr_val_index(from_index, val):
+        for i in range(from_index, len(array)):
+            if array[i] == val:
+                return i
+        return -1
+
+
+    while s_pointer < len(sequence):
+        s_value = sequence[s_pointer]
+        r_pointer = arr_val_index(r_pointer, s_value)
+        # print((r_pointer), (s_pointer, s_value))
+        if r_pointer == -1:
+            break
+        s_pointer+=1
+        r_pointer+=1
+
+        if s_pointer == len(sequence):
+            return True
+
+    return False
+
+def isValidSubsequence_sol_2(array, sequence):
+    """
+    Single iteration
+    """
+    a_index = 0
+    s_index = 0
+    while a_index < len(array):
+
+        a_value = array[a_index]
+        s_value = sequence[s_index]
+        print((a_index, a_value), (s_index, s_value))
+
+        if a_value == s_value:
+            s_index+=1
+
+        a_index+=1
+        if s_index == len(sequence):
+            return True
+
+    return False
---
+++ src/algoexpert/easy/test_t_02_validate_subsequence.py
@@ -0,0 +1,43 @@
+import pytest
+from src.algoexpert.easy.t_02_validate_subsequence import isValidSubsequence_sol_1, isValidSubsequence_sol_2
+
+test_cases = [
+    ([5, 1, 22, 25, 6, -1, 8, 10], [1, 6, -1, 10], True),
+    ([5, 1, 22, 25, 5, -1, 8, 10], [1, 6, -1, 10], False),
+    ([], [1, 6, -1, 10], False),
+    ([5], [1, 6, -1, 10], False),
+    ([1, 2, 3, 4], [1, 3, 4], True),
+    ([1, 2, 3, 4], [2, 4], True),
+    ([1, 2, 3, 4], [1], True),
+    ([1, 2, 3, 4], [2], True),
+    ([1, 2, 3, 4], [4], True),
+    ([5, 1, 22, 25, 6, -1, 8, 10], [5, 1, 22, 25, 6, -1, 8, 10], True),
+    ([5, 1, 22, 25, 6, -1, 8, 10], [5, 1, 22, 6, -1, 8, 10], True),
+    ([5, 1, 22, 25, 6, -1, 8, 10], [22, 25, 6], True),
+    ([5, 1, 22, 25, 6, -1, 8, 10], [1, 6, 10], True),
+    ([5, 1, 22, 25, 6, -1, 8, 10], [5, 1, 22, 10], True),
+    ([5, 1, 22, 25, 6, -1, 8, 10], [5, -1, 8, 10], True),
+    ([5, 1, 22, 25, 6, -1, 8, 10], [25], True),
+    ([1, 1, 1, 1, 1], [1, 1, 1], True),
+    ([5, 1, 22, 25, 6, -1, 8, 10], [5, 1, 22, 25, 6, -1, 8, 10, 12], False),
+    ([5, 1, 22, 25, 6, -1, 8, 10], [4, 5, 1, 22, 25, 6, -1, 8, 10], False),
+    ([5, 1, 22, 25, 6, -1, 8, 10], [5, 1, 22, 22, 25, 6, -1, 8, 10], False),
+    ([5, 1, 22, 25, 6, -1, 8, 10], [1, 6, -1, -1], False),
+    ([5, 1, 22, 25, 6, -1, 8, 10], [1, 6, -1, -1, 10], False),
+    ([5, 1, 22, 25, 6, -1, 8, 10], [1, 6, -1, -2], False),
+    ([5, 1, 22, 25, 6, -1, 8, 10], [26], False),
+    ([5, 1, 22, 25, 6, -1, 8, 10], [5, 26, 22, 8], False),
+    ([5, 1, 22, 25, 6, -1, 8, 10], [1, 1, 1, 6], False),
+    ([5, 1, 22, 25, 6, -1, 8, 10], [1, 6, -1, 10, 11, 11, 11, 11], False),
+    ([5, 1, 22, 25, 6, -1, 8, 10], [5, 1, 22, 25, 6, -1, 8, 10, 10], False),
+    ([5, 1, 22, 25, 6, -1, 8, 10], [1, 6, -1, 5], False),
+]
+
+
+@pytest.mark.parametrize("input_array, input_sequence, result", test_cases)
+def test_isValidSubsequence_sol_1(input_array, input_sequence, result):
+    assert isValidSubsequence_sol_1(input_array, input_sequence) == result
+
+@pytest.mark.parametrize("input_array, input_sequence, result", test_cases)
+def test_isValidSubsequence_sol_2(input_array, input_sequence, result):
+    assert isValidSubsequence_sol_2(input_array, input_sequence) == result
