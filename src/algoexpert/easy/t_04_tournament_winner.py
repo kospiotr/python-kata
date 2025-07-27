36f9f9b9d29c5a579f2c675927558a5c16de12ad
Piotr Kosmowski <kospiotr@gmail.com>
2025-07-27 15:17:47 +0200

    Update

--- src/algoexpert/easy/t_04_tournament_winner.py
+++ src/algoexpert/easy/t_04_tournament_winner.py
@@ -1,4 +1,8 @@
 """
+Difficulty: Easy
+Category: Arrays
+Tournament Winner
+
 There's an algorithms tournament taking place in which teams of programmers
 compete against each other to solve algorithmic problems as fast as possible.
 Teams compete in a round robin, where each team faces off against all other
---
+++ test/algoexpert/easy/test_t_05_non_constructibe_change.py
@@ -0,0 +1,30 @@
+import pytest
+
+from src.algoexpert.easy.t_05_non_constructibe_change import nonConstructibleChange_sol_1
+
+
+@pytest.mark.parametrize("coins, result_expected", [
+    ([5, 7, 1, 1, 2, 3, 22], 20),
+    ([3], 1),
+    ([1], 2),
+    ([1, 2], 4),
+    ([1, 2, 7], 4),
+    ([1, 2, 3], 7),
+    ([1, 2, 3, 7], 14),
+    ([5, 7, 1, 1, 2, 3, 22], 20),
+    ([1, 1, 1, 1, 1], 6),
+    ([1, 5, 1, 1, 1, 10, 15, 20, 100], 55),
+    ([6, 4, 5, 1, 1, 8, 9], 3),
+    ([], 1),
+    ([87], 1),
+    ([5, 6, 1, 1, 2, 3, 4, 9], 32),
+    ([5, 6, 1, 1, 2, 3, 43], 19),
+    ([1, 1], 3),
+    ([2], 1),
+    ([1], 2),
+    ([109, 2000, 8765, 19, 18, 17, 16, 8, 1, 1, 2, 4], 87),
+    ([1, 2, 3, 4, 5, 6, 7], 29)
+
+])
+def test_nonConstructibleChange_sol_1(coins, result_expected):
+    assert nonConstructibleChange_sol_1(coins) == result_expected
